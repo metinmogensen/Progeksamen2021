aa76fd4e6f39d8fc2e1252fb0bedeb622c0d8112
Metin <metin.mogensen97@gmail.com>
2021-03-24 18:45:11 +0100

    ss

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express')
-const app = new express()
-app.listen(3000,() => {
-    console.log('listening on port 3000');
-})
+const path = require('path')
+
+const app = express()
+app.get('/register', (req, res,) => {
+    res.sendFile(path.resolve(__dirname,'veiws/register.html'))
+    });
+
+app.get('/login', (req, res, next) => {
+    console.log('in another middleware');
+    res.send('<h1> Hello fra Express<h1>')
+    });
+
+app.get('/', (req, res, next) => {
+    console.log('in another middleware');
+    res.send('<h1> Hello fra Express<h1>')
+    });
+    
+
+app.listen(3000)
+
 
