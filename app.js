c23b9d106a8a0917a61a3c9951f36408c997b642
Metin <metin.mogensen97@gmail.com>
2021-03-24 19:26:49 +0100

    ss

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const path = require('path')
 
-const app = express()
+app.use(bodyParser.urlencoded({extended:fal}))
+
+const app = express() 
+
 app.get('/register', (req, res,) => {
     res.sendFile(path.resolve(__dirname,'veiws/register.html'))
     });
 
+app.post('/users/regiser', (req, res,) => {
+    console.log(req.body);
+    res.redirect('/register')
+    });
+    
 app.get('/login', (req, res, next) => {
     console.log('in another middleware');
     res.send('<h1> Hello fra Express<h1>')
