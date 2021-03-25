9f1a11bf10aca08d93e396649755fd72318aee1b
Metin <metin.mogensen97@gmail.com>
2021-03-25 20:27:22 +0100

    kk

--- routes/Homepage.js
+++ routes/Homepage.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router();
+const path = require('path');
 
 
 router.get('/', (req, res,) => {
-    res.send('<h1> Hello fra Express<h1>')
+    res.sendfile(path.resolve(__dirname,'/Users/metinyucel/Desktop/Progeksamen2021/Progeksamen2021/veiws/homepage.html'))
     });
     
 module.exports = router; 
