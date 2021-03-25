447dd3a5e2e086fdbe78dbe81d4d6287b053bd4e
Metin <metin.mogensen97@gmail.com>
2021-03-25 20:16:46 +0100

    kk

--- routes/register.js
+++ routes/register.js
@@ -3,13 +3,15 @@ const path = require('path');
 
 const router = express.Router();
 
-router.get('/register', (req, res, next) => {
+router.post('/posttest', (req, res ) => {
+    console.log(req.body);
+     res.redirect('/');
+     });
+
+router.get('/register', (req, res) => {
+    //res.send('<form action="/posttest" method="post"><input type="text" name="username" placeholder="Username"><input type="text" name="password" placeholder="Password"><button type="submit">Add user</form>')
     res.sendFile(path.resolve(__dirname,'/Users/metinyucel/Desktop/Progeksamen2021/Progeksamen2021/veiws/register.html'));
     });
 
-router.post('/regiser/users', (req, res ) => {
-    console.log(req.body);
-    res.redirect('/');
-    });
-    
+
 module.exports = router;
