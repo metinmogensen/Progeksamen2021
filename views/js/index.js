024e163b2f98a7d5c33b84f6ccd25c9f844de336
Your Name <you@example.com>
2021-04-18 12:15:33 +0200

    klklk

--- views/js/index.js
+++ views/js/index.js
@@ -1,6 +1,6 @@
 var form = document.getElementById("form")
 
-form.addEventListener("submit", function(e) {
+form.addEventListener('submit', function(e) {
     e.preventDefault()
 
     var name = document.getElementById("name").value
@@ -9,5 +9,47 @@ form.addEventListener("submit", function(e) {
     var country = document.getElementById("country").value
     var birthdate = document.getElementById("birthdate").value
 
-    fetch("http://localhost:7071/api/HttpTrigger1")
+    fetch("http://localhost:7071/api/user", {
+        method: 'POST',
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            gender: gender, 
+            country: country,
+            birthdate: birthdate
+        }), 
+        headers: {
+            "Content-Type": "application/json; charset-UTF-8"
+        }
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data)
+    }).catch((err) =>{
+        console.log(err)
+    })
+})
+
+var getButton = document.getElementById("getUsers")
+
+getButton.addEventListener("click", function(){
+    var name1 = document.getElementById("name").value
+    fetch(`http://localhost:7071/api/user?name=${name1}`)
+        .then(
+            function(response){
+                if (response.status !== 200){
+                    console.log("Noget gik galt" + response.status);
+                    return;
+                }
+
+                response.json().then(function (data) {
+                    console.log(data);
+                });
+            }
+        )
+        .catch(function (err){
+            console.log(err);
+        });
 })
