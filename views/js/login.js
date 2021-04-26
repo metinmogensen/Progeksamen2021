46840d5266b64e0070b2203d376b5362ebf34600
Metin <metin.mogensen97@gmail.com>
2021-04-26 16:48:46 +0200

    Update login.js

---
+++ views/js/login.js
@@ -0,0 +1,36 @@
+
+
+var form = document.getElementById("form")
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    var firstName = document.getElementById("firstName").value
+    var password = document.getElementById("password").value
+
+
+    fetch("http://localhost:7071/api/register2", {
+        method: 'POST',
+        body: JSON.stringify({
+            firstName: firstName,
+            lastName: lastName,
+            gender: gender, 
+            email: email,
+            password: password,
+            age: age,
+            hotel: hotel,
+            preferredGender: preferredGender
+        }), 
+        headers: {
+            "Content-Type": "application/json; charset-UTF-8"
+        }
+    })
+    .then((response) => {
+        return response.json()//vores server bruger tekst -> så det burde være res.tekst
+    })
+    .then((data) => {
+        console.log(data)
+    }).catch((err) =>{ // catcher fejl, hvis noget går galt
+        console.log("wuups: " + err)
+    })
