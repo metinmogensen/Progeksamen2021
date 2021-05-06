6bec7ec4c1ca661700c063d346b9d9aecf4b0c62
Metin <metin.mogensen97@gmail.com>
2021-05-06 18:13:52 +0200

    sada

--- client3/src/components/Login.vue
+++ client3/src/components/Login.vue
@@ -40,58 +40,32 @@ export default {
       password: ""
     };
   },
-  // methods: {
-  //   onSubmit() {
-  //     axios
-  //       .post("http://127.0.0.1:7071/api/login/", {
-  //         email: this.email,
-  //         password: this.password,
-  //       })
-  //       .then((response) => {
-  //         if (response.data.accessToken){
-  //           localStorage.setItem('user', JSON.stringify(response.data));
-  //         }
-
-  //         return response.data
-
-
-  //         //redirect logic
-  //         if (response.status == 200) {
-  //           alert("Now login after u clicked OK!")
-  //           this.$router.push({ path : '/profile' });
-  //         }
+  methods: {
+    onSubmit() {
+      axios
+        .post("http://127.0.0.1:7071/api/login/", {
+          email: this.email,
+          password: this.password,
+        })
+        .then((response) => {
           
-  //       })
-  //       .catch((error) => {
-  //         alert("Fejl forkert brugernavn eller password")
-  //         console.log(error);
-  //       });
-  //   },
-  // },
+          //redirect logic
+           if (response.status == 200) {
+             localStorage.setItem("email", response.data);
+             alert("Now login after u clicked OK!")
+             this.$router.push({ path : '/profile' });
+           }
+          
+         })
+         .catch((error) => {
+           alert("Fejl forkert brugernavn eller password")
+           console.log(error);
+         });
+     },
+   },
 };
 
 
-
-
-// // Add a request interceptor
-// axios.interceptors.request.use(function (config) {
-//     // Do something before request is sent
-//     return config;
-//   }, function (error) {
-//     // Do something with request error
-//     return Promise.reject(error);
-//   });
-
-// // Add a response interceptor
-// axios.interceptors.response.use(function (response) {
-//     // Any status code that lie within the range of 2xx cause this function to trigger
-//     // Do something with response data
-//     return response;
-//   }, function (error) {
-//     // Any status codes that falls outside the range of 2xx cause this function to trigger
-//     // Do something with response error
-//     return Promise.reject(error);
-//   });
 
 
 </script>
